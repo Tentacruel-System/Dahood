4b881f0e1836d1f49ca475de0fce3a06c3afc9f9
jmct1208 <jesus@700-15ISK>
2017-10-19 18:08:51 -0500

    modified:   dahood.sql

--- dahood.sql
+++ dahood.sql
@@ -1,61 +1,65 @@
-DROP TABLE IF EXISTS Gusto, Usuario, Chat, Usuario_gusto;
+DROP TABLE IF EXISTS usuario, usuario_gusto, chat, gusto;
 
 
-CREATE TABLE Gusto (
+CREATE TABLE gusto (
                 id_gusto INT NOT NULL AUTO_INCREMENT,
                 nombre_gusto VARCHAR(40) NOT NULL,
                 CONSTRAINT id_gusto PRIMARY KEY (id_gusto)
 );
 
 
-CREATE TABLE Usuario (
+CREATE TABLE usuario (
                 id_usuario INT NOT NULL,
                 contrasena VARCHAR(40) NOT NULL,
                 foto VARCHAR(100) NOT NULL,
                 nombre VARCHAR(40) NOT NULL,
                 apellido_p VARCHAR(40) NOT NULL,
                 apellido_m VARCHAR(40) NOT NULL,
-                CONSTRAINT id_Usuario PRIMARY KEY (id_usuario)
+		nickname VARCHAR(40) NOT NULL,
+		correo VARCHAR(40) NOT NULL,
+                PRIMARY KEY(id_usuario),
+		UNIQUE (nickname, correo)
+		
 );
 
 
-CREATE TABLE Chat (
+CREATE TABLE chat (
                 id_chat INT NOT NULL AUTO_INCREMENT,
                 id_usuario1 INT,
                 id_usuario2 INT,
                 fecha DATE NOT NULL,
                 ruta VARCHAR(100) NOT NULL,
-                CONSTRAINT chat_pk PRIMARY KEY (id_chat)
+                PRIMARY KEY (id_chat)
 );
 
 
-CREATE TABLE Usuario_gusto (
+CREATE TABLE usuario_gusto (
                 id_gusto INT NOT NULL,
                 id_usuario INT NOT NULL,
-                CONSTRAINT usuario_gusto_pk PRIMARY KEY (id_gusto, id_usuario)
+                PRIMARY KEY (id_gusto, id_usuario)
 );
 
 
-ALTER TABLE Usuario_gusto ADD CONSTRAINT Gusto_Usuario_gusto_fk
+ALTER TABLE usuario_gusto ADD CONSTRAINT gusto_usuario_gusto_fk
 FOREIGN KEY (id_gusto)
-REFERENCES Gusto (id_gusto)
+REFERENCES gusto (id_gusto)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE Usuario_gusto ADD CONSTRAINT Usuario_Usuario_gusto_fk
+ALTER TABLE usuario_gusto ADD CONSTRAINT usuario_usuario_gusto_fk
 FOREIGN KEY (id_usuario)
-REFERENCES Usuario (id_usuario)
+REFERENCES usuario (id_usuario)
 ON DELETE CASCADE
 ON UPDATE NO ACTION;
 
-ALTER TABLE Chat ADD CONSTRAINT Usuario_chat_fk
+ALTER TABLE chat ADD CONSTRAINT usuario_chat_fk
 FOREIGN KEY (id_usuario1)
-REFERENCES Usuario (id_usuario)
+REFERENCES usuario (id_usuario)
 ON DELETE SET NULL
 ON UPDATE NO ACTION;
 
-ALTER TABLE Chat ADD CONSTRAINT Usuario_chat_fk1
+ALTER TABLE chat ADD CONSTRAINT usuario_chat_fk1
 FOREIGN KEY (id_usuario2)
-REFERENCES Usuario (id_usuario)
+REFERENCES usuario (id_usuario)
 ON DELETE SET NULL
 ON UPDATE NO ACTION;
