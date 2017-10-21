0edf1b40104e0cf76d8b35edd63970a2bfedafaa
Leonardo Gallo <lgallo@ciencias.unam.mx>
2017-10-20 20:03:52 -0500

    Clase Usuario terminada

--- src/main/java/com/tentacruel/dahood/mapeobd/Usuario.java
+++ src/main/java/com/tentacruel/dahood/mapeobd/Usuario.java
@@ -5,38 +5,29 @@
  */
 package com.tentacruel.dahood.mapeobd;
 
-import java.io.Serializable;
-import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 
 /**
  * Hacemos el mapeo de un elemento de la tabla Usuario a un objeto Usuario
  * @author lgallo
  */
 @Entity
-@Table(name = "Usuario")
+@Table(name = "usuario")
 public class Usuario {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
     private int id_usuario;
-    
+           
     @Column(name = "nickname")
     private String nickname;
             
     @Column(name = "nombre")
     private String nombre;
-    
-    @Column(name = "correo")
-    private String correo;
-    
-    @Column(name = "contrasena")
-    private String contrasena;
     
     @Column(name = "apellido_p")
     private String apellido_p;
@@ -44,9 +35,15 @@ public class Usuario {
     @Column(name = "apellido_m")
     private String apellido_m;
 
+    @Column(name = "contrasena")
+    private String contrasena;
+    
     @Column(name = "foto")
     private String foto;
 
+    @Column(name = "correo")
+    private String correo;
+ 
     //parece que es la relación establecida con otras tablas
     //@OneToMany(mappedBy = "Usuario") //esto para qué?
     //private Set<Marcador> marcadores; //es de la tabla o la base?
@@ -55,7 +52,7 @@ public class Usuario {
      * Permite obtener el id del usuario
      * @return  id_usuario
      */
-    public int getIdusuario() {
+    public int getIdUsuario() {
         return id_usuario;
     }
 
@@ -63,10 +60,26 @@ public class Usuario {
      * Permite cambiar el id del usuario
      * @param   id_usuario
      */
-    public void setIdusuario(int id_usuario) {
+    public void setIdUsuario(int id_usuario) {
         this.id_usuario = id_usuario;
     }
+    
+    /**
+     * 
+     * @return devuelve el nickname actual del usuario
+     */
+    public String getNickname() {
+        return nickname;
+    }
 
+    /**
+     * Cambia el nickname del usuario
+     * @param nickname 
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    
     /**
      * Permite obtener el nombre del usuario
      * @return  nickname
@@ -84,12 +97,35 @@ public class Usuario {
     }
 
     /**
-     * Devuelve la contraseña actual del usuario
-     * este método no da seguridad!!!
-     * @return contrasena
+     * Permite obtener el apellido paterno de usuario
+     * @return apellido_p
      */
-    public String getContrasena() {
-        return contrasena;
+    public String getApellidoPaterno() {
+        return apellido_p;
+    }
+
+    /**
+     * Permite configurar el apellido paterno de usuario
+     * @param apellido_p 
+     */
+    public void setApellidoPaterno(String apellido_p) {
+        this.apellido_p = apellido_p;
+    }
+
+    /**
+     * 
+     * @return devuelve el apellido materno del usuario
+     */
+    public String getApellidoMaterno() {
+        return apellido_m;
+    }
+
+    /**
+     * Cambia el apellido materno del usuario
+     * @param apellido_m 
+     */
+    public void setApellidoMaterno(String apellido_m) {
+        this.apellido_m = apellido_m;
     }
 
     /**
@@ -101,51 +137,35 @@ public class Usuario {
     }
     
     /**
-     * Permite obtener el apellido paterno de usuario
-     * @return apellido_p
-     */
-    public String getApellido_p() {
-        return apellido_p;
-    }
-
-    /**
-     * Permite configurar el apellido paterno de usuario
-     * @param apellido_p 
-     */
-    public void setApellido_p(String apellido_p) {
-        this.apellido_p = apellido_p;
-    }
-
-    /**
      * 
-     * @return 
-     */
-    public String getApellido_m() {
-        return apellido_m;
-    }
-
-    /**
-     * 
-     * @param apellido_m 
-     */
-    public void setApellido_m(String apellido_m) {
-        this.apellido_m = apellido_m;
-    }
-
-    /**
-     * 
-     * @return 
+     * @return devuelve la ruta de la foto del perfil del usuario
      */
     public String getFoto() {
         return foto;
     }
 
     /**
-     * 
+     * Permite cambiar la foto del perfil de usuario
      * @param foto 
      */
     public void setFoto(String foto) {
         this.foto = foto;
-    }    
+    }
+
+    /**
+     * 
+     * @return devuelve el correo del usuario
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * Cambia el correo del usuario
+     * @param correo 
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 }
 
