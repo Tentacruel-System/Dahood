ed44953188fa05aeb3fe90012f27200f011a09c6
Mauricio <maw@ciencias.unam.mx>
2017-10-26 23:07:18 -0500

    Tabla conocer agregada

--- dahood.sql
+++ dahood.sql
@@ -1,65 +1,162 @@
-DROP TABLE IF EXISTS usuario, usuario_gusto, chat, gusto;
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: dahood
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.16.04.1
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE gusto (
-                id_gusto INT NOT NULL AUTO_INCREMENT,
-                nombre_gusto VARCHAR(40) NOT NULL,
-                CONSTRAINT id_gusto PRIMARY KEY (id_gusto)
-);
+--
+-- Table structure for table `chat`
+--
 
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
+  `id_chat` int(11) NOT NULL AUTO_INCREMENT,
+  `id_usuario1` int(11) DEFAULT NULL,
+  `id_usuario2` int(11) DEFAULT NULL,
+  `fecha` date NOT NULL,
+  `ruta` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_chat`),
+  KEY `usuario_chat_fk` (`id_usuario1`),
+  KEY `usuario_chat_fk1` (`id_usuario2`),
+  CONSTRAINT `usuario_chat_fk` FOREIGN KEY (`id_usuario1`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  CONSTRAINT `usuario_chat_fk1` FOREIGN KEY (`id_usuario2`) REFERENCES `usuario` (`id_usuario`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE usuario (
-                id_usuario INT NOT NULL,
-                contrasena VARCHAR(40) NOT NULL,
-                foto VARCHAR(100) NOT NULL,
-                nombre VARCHAR(40) NOT NULL,
-                apellido_p VARCHAR(40) NOT NULL,
-                apellido_m VARCHAR(40) NOT NULL,
-		nickname VARCHAR(40) NOT NULL,
-		correo VARCHAR(40) NOT NULL,
-                PRIMARY KEY(id_usuario),
-		UNIQUE (nickname, correo)
-		
-);
+--
+-- Dumping data for table `chat`
+--
 
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE chat (
-                id_chat INT NOT NULL AUTO_INCREMENT,
-                id_usuario1 INT,
-                id_usuario2 INT,
-                fecha DATE NOT NULL,
-                ruta VARCHAR(100) NOT NULL,
-                PRIMARY KEY (id_chat)
-);
+--
+-- Table structure for table `conocer`
+--
 
+DROP TABLE IF EXISTS `conocer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conocer` (
+  `usuario` int(11) NOT NULL,
+  `amigo` int(11) NOT NULL,
+  PRIMARY KEY (`usuario`,`amigo`),
+  KEY `amigo2` (`amigo`),
+  CONSTRAINT `conocer_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `conocer_ibfk_2` FOREIGN KEY (`amigo`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE usuario_gusto (
-                id_gusto INT NOT NULL,
-                id_usuario INT NOT NULL,
-                PRIMARY KEY (id_gusto, id_usuario)
-);
+--
+-- Dumping data for table `conocer`
+--
 
+LOCK TABLES `conocer` WRITE;
+/*!40000 ALTER TABLE `conocer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conocer` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE usuario_gusto ADD CONSTRAINT gusto_usuario_gusto_fk
-FOREIGN KEY (id_gusto)
-REFERENCES gusto (id_gusto)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Table structure for table `gusto`
+--
 
-ALTER TABLE usuario_gusto ADD CONSTRAINT usuario_usuario_gusto_fk
-FOREIGN KEY (id_usuario)
-REFERENCES usuario (id_usuario)
-ON DELETE CASCADE
-ON UPDATE NO ACTION;
+DROP TABLE IF EXISTS `gusto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gusto` (
+  `id_gusto` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_gusto` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_gusto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE chat ADD CONSTRAINT usuario_chat_fk
-FOREIGN KEY (id_usuario1)
-REFERENCES usuario (id_usuario)
-ON DELETE SET NULL
-ON UPDATE NO ACTION;
+--
+-- Dumping data for table `gusto`
+--
 
-ALTER TABLE chat ADD CONSTRAINT usuario_chat_fk1
-FOREIGN KEY (id_usuario2)
-REFERENCES usuario (id_usuario)
-ON DELETE SET NULL
-ON UPDATE NO ACTION;
+LOCK TABLES `gusto` WRITE;
+/*!40000 ALTER TABLE `gusto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gusto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `contrasena` varchar(40) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `apellido_p` varchar(40) NOT NULL,
+  `apellido_m` varchar(40) NOT NULL,
+  `nickname` varchar(40) NOT NULL,
+  `correo` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `nickname` (`nickname`,`correo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_gusto`
+--
+
+DROP TABLE IF EXISTS `usuario_gusto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario_gusto` (
+  `id_gusto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  PRIMARY KEY (`id_gusto`,`id_usuario`),
+  KEY `usuario_usuario_gusto_fk` (`id_usuario`),
+  CONSTRAINT `gusto_usuario_gusto_fk` FOREIGN KEY (`id_gusto`) REFERENCES `gusto` (`id_gusto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `usuario_usuario_gusto_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_gusto`
+--
+
+LOCK TABLES `usuario_gusto` WRITE;
+/*!40000 ALTER TABLE `usuario_gusto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario_gusto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-26 22:50:56
